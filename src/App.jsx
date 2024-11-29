76a29e6adced9978569319b51c6ae3cf5e2fd94d
tantonPeerapat <peerapatton2602@gmail.com>
2024-11-29 21:43:56 +0700

    create Newsletter Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Analytics from "./components/Analytics";
+import Newsletter from "./components/Newsletter";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </>
   );
 };

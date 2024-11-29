a1f79af4c95c272c772defd96349f08410b8f465
tantonPeerapat <peerapatton2602@gmail.com>
2024-11-29 22:04:27 +0700

    create Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Analytics from "./components/Analytics";
 import Newsletter from "./components/Newsletter";
 import Card from "./components/Card";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Analytics />
       <Newsletter />
       <Card />
+      <Footer />
     </>
   );
 };
